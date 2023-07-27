f0c6fbe0f7d96c6ca9de9c49f0f48d8dd038dcfc
mufaqar <mufaqar@gmail.com>
2023-07-27 21:32:46 +0500

    updated

---
+++ src/pages/series/index.tsx
@@ -0,0 +1,50 @@
+
+import React from 'react'
+import { Helmet } from 'react-helmet';
+export default function Series() {
+    return (
+        <>
+            <Helmet>
+                <title>Program Scheduling - Paigham TV</title>
+                <meta name="description" content="Paigham TV is an Islamic educational channel television network. It launched its Urdu language TV channel in 2011. The production of this channel is based on the teachings of Quran o Sunnah. A team of Islamic scholars monitors the material presented on its programs. Each program of Paigham TV is previewed according to authentic references of Quran o Hadees." />
+                <link rel="canonical" href="https://paigham.tv/" />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Home - Paigham TV" />
+                <meta property="og:description" content="Paigham TV is an Islamic educational channel television network. It launched its Urdu language TV channel in 2011. The production of this channel is based on the teachings of Quran o Sunnah. A team of Islamic scholars monitors the material presented on its programs. Each program of Paigham TV is previewed according to authentic references of Quran o Hadees." />
+                <meta property="og:url" content="https://paigham.tv/" />
+                <meta property="og:site_name" content="Paigham TV" />
+                <meta property="article:publisher" content="https://web.facebook.com/paighamtv" />
+                <meta property="article:modified_time" content="2023-05-14T04:41:56+00:00" />
+                <meta property="og:image" content="https://paigham.tv/wp-content/uploads/2018/09/paigham-tv-logo-img.jpg" />
+                <meta property="og:image:width" content="960" />
+                <meta property="og:image:height" content="960" />
+                <meta property="og:image:type" content="image/jpeg" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@paighamtv" />
+                <meta name="twitter:label1" content="Est. reading time" />
+                <meta name="twitter:data1" content="2 minutes" />
+            </Helmet>
+
+
+            {/* Categories section  */}
+            <section className='container mx-auto mb-28 px-4'>
+                {/* heading  */}
+                <div className=' justify-between items-center mt-20 mb-10  '>
+                    <h2 className='font-metapro text-3xl md:text-5xl text-darkgray font-bold border-b-[3px] border-darkgray  pb-5'>Series </h2>
+                    <div className='pt-6'>
+                    </div>
+                </div>
+
+            </section>
+
+
+
+        </>
+    )
+}
+
+
+
+
+
