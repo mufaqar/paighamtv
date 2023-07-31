b34cd7be912db4c44be75e92e22de888fc1f637e
syed zunair gillani <gillanix007@gmail.com>
2023-07-31 16:27:43 +0500

    update model close button

--- src/components/model-box/VideoModelBox.tsx
+++ src/components/model-box/VideoModelBox.tsx
@@ -11,8 +11,10 @@ const VideoModelBox = ({ setModelIsOpen }: any) => {
     <>
     <div className='bg-black/60 fixed inset-0 z-50'>
       <div className='relative w-full h-full'>
-        <button className="absolute top-6 right-6" onClick={closeModal}><MdOutlineClose size={35} color="white"/></button>
-        <div className='max-w-[850px] mx-auto h-full flex px-4 flex-col justify-center'><VideoPlayer/></div>
+        <div className='max-w-[850px] relative mx-auto h-full flex px-4 flex-col justify-center'>
+        <button className="flex justify-end items-end mb-3" onClick={closeModal}><MdOutlineClose size={35} color="white"/></button>
+          <VideoPlayer/>
+          </div>
       </div>
     </div>
     </>
