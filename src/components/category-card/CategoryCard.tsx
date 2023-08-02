7a2db3b609e6d027862c67b41bf72b223c30b935
mufaqar <mufaqar@gmail.com>
2023-08-02 20:52:14 +0500

    Default

--- src/components/category-card/CategoryCard.tsx
+++ src/components/category-card/CategoryCard.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 import React from 'react'
-import DefaultImage from '../../../public/images/Islamic.png'
+//import DefaultImage from '../../../public/images/Islamic.png'
 import Link from 'next/link'
-
 
 const CategoryCard = ({ item }: any) => {
   const featued_image = item?.categoryInfo?.catImage?.mediaItemUrl;
-
   return (
     <div className='bg-red-300 relative group overflow-hidden'>
       <Link href={`/category/${item.slug}`} ><Image src={featued_image} alt="image" width={700} height={700} className='w-full object-cover h-[420px] transition-all duration-200 ease-in-out group-hover:scale-105' />
@@ -18,5 +16,4 @@ const CategoryCard = ({ item }: any) => {
     </div>
   )
 }
-
 export default CategoryCard
