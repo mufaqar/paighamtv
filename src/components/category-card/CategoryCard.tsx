45b93b73ddde018cd80faa92cf7e123bdbe880b0
mufaqar <mufaqar@gmail.com>
2023-08-02 20:46:23 +0500

    updated

--- src/components/category-card/CategoryCard.tsx
+++ src/components/category-card/CategoryCard.tsx
@@ -5,9 +5,11 @@ import Link from 'next/link'
 
 
 const CategoryCard = ({ item }: any) => {
+  const featued_image = item?.categoryInfo?.catImage?.mediaItemUrl;
+
   return (
     <div className='bg-red-300 relative group overflow-hidden'>
-      <Link href={`/category/${item.slug}`} ><Image src={item?.categoryInfo?.catImage?.mediaItemUrl || DefaultImage} alt="image" width={700} height={700} className='w-full object-cover h-[420px] transition-all duration-200 ease-in-out group-hover:scale-105' />
+      <Link href={`/category/${item.slug}`} ><Image src={featued_image} alt="image" width={700} height={700} className='w-full object-cover h-[420px] transition-all duration-200 ease-in-out group-hover:scale-105' />
         <div className='bg-gradient-to-t from-primary absolute inset-0 p-6 flex flex-col justify-end font-metapro to-black/0'>
           <h4 className='text-white text-2xl font-semibold tracking-wide'>{item.name}</h4>
           <p className='text-orange font-medium text-lg'>{item.count} Shows</p>
