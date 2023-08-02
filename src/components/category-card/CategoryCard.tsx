6483992a68cecffa7927c5c271cfb161e5a6260f
mufaqar <mufaqar@gmail.com>
2023-08-02 19:47:25 +0500

    Categoires

--- src/components/category-card/CategoryCard.tsx
+++ src/components/category-card/CategoryCard.tsx
@@ -1,16 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 import DefaultImage from '../../../public/images/Islamic.png'
+import Link from 'next/link'
 
 
 const CategoryCard = ({ item }: any) => {
   return (
     <div className='bg-red-300 relative group overflow-hidden'>
-      <Image src={item?.categoryInfo?.catImage?.mediaItemUrl || DefaultImage} alt="image" width={700} height={700} className='w-full object-cover h-[420px] transition-all duration-200 ease-in-out group-hover:scale-105' />
-      <div className='bg-gradient-to-t from-primary absolute inset-0 p-6 flex flex-col justify-end font-metapro to-black/0'>
-        <h4 className='text-white text-2xl font-semibold tracking-wide'>{item.name}</h4>
-        <p className='text-orange font-medium text-lg'>{item.count} Shows</p>
-      </div>
+      <Link href={`/category/${item.slug}`} ><Image src={item?.categoryInfo?.catImage?.mediaItemUrl || DefaultImage} alt="image" width={700} height={700} className='w-full object-cover h-[420px] transition-all duration-200 ease-in-out group-hover:scale-105' />
+        <div className='bg-gradient-to-t from-primary absolute inset-0 p-6 flex flex-col justify-end font-metapro to-black/0'>
+          <h4 className='text-white text-2xl font-semibold tracking-wide'>{item.name}</h4>
+          <p className='text-orange font-medium text-lg'>{item.count} Shows</p>
+        </div>
+      </Link>
     </div>
   )
 }
