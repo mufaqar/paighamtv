b0e15c76f7f05ab35fcbad06d09841b689aa3c31
mufaqar <mufaqar@gmail.com>
2023-08-18 16:23:07 +0500

    Socials

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa'
+import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter, FaTiktok } from 'react-icons/fa'
 import { HiOutlineArrowRight } from 'react-icons/hi'
 import Logo from '../logo/Logo'
 import { footerNav } from '../../../public/data'
@@ -48,10 +48,11 @@ export default Footer
 export const Socials = () => {
      return (
           <div className="flex justify-center sm:justify-start space-x-5 text-2xl ">
-               <Link href="http://facebook.com/paighamtv" target='_blank' className='hover:text-secondary'><FaFacebookF size={21}/></Link>
+               <Link href="http://facebook.com/paighamtv" target='_blank' className='hover:text-secondary'><FaFacebookF size={21} /></Link>
                <Link href="http://instgram.com/paighamtv" target='_blank' className='hover:text-secondary'><FaInstagram /></Link>
                <Link href="http://youtube.com/paighamtvofficial" target='_blank' className='hover:text-secondary'><FaYoutube /></Link>
                <Link href="http://twitter.com/paighamtv" target='_blank' className='hover:text-secondary'><FaTwitter /></Link>
+               <Link href="https://www.tiktok.com/@paigham_tv" target='_blank' className='hover:text-secondary'><FaTiktok /></Link>
           </div>
      )
 }
