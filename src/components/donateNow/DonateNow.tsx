2adeea821cbf42b33623bc32f8d9a7e4f3ba3875
mufaqar <mufaqar@gmail.com>
2023-11-21 14:37:30 +0500

    donates Us

--- src/components/donateNow/DonateNow.tsx
+++ src/components/donateNow/DonateNow.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const DonateNow = () => {
   return (
-    <section className='bg-[url("/images/donate-now.png")] bg-cover bg-no-repeat py-32'>
+    <section className='bg-[url("/images/donate-now.jpg")] bg-cover bg-no-repeat py-32'>
        <div className='container mx-auto px-4'>
        <h2 className='font-metapro text-white text-4xl md:text-5xl max-w-[600px] md:leading-[60px] tracking-widest font-semibold'>The Easy Way To Donate To Good Causes</h2>
         <p className="text-white max-w-[500px] mt-2 font-metapro tracking-wider mb-10">Make one-off or monthly donations. Every penny goes direct to your favorite's charities.</p>
